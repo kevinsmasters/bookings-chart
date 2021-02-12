fb8c970eb9ab4f710a39ac54862adca2a24397de
Kevin Masters <kevin.masters@taconic.com>
2021-02-12 16:16:38 -0500

    added booking data scheme

--- components/BookingList.js
+++ components/BookingList.js
@@ -1,14 +1,26 @@
 import BookingItem from './BookingItem'
 
 const BookingList = ({bookings}) =>{
-    
+
+    let dailyArr = [];
+
+    for (let item in bookings) {
+        let tempItem = {date: "",booking: 0};
+
+        tempItem.date = bookings[item]['label']
+
+
+        tempItem.booking = bookings[item]['amount']
+
+
+
+        dailyArr.push(tempItem)
+    }
+
+console.log(dailyArr)
     return(
         <div>
-            {bookings.map(booking => (
-                <BookingItem booking={booking} />
-            )
-
-            )}
+            dailyArr
         </div>
     )
 }
---
+++ pages/api/cancellations.js
@@ -0,0 +1,5 @@
+const bookings = require('../../sf_booking_cancellation_report');
+
+export default (req, res) => {
+  res.status(200).json(bookings.cancellations.data)
+}
